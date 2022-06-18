f1f6d317677a5b86196e84e34db5d700181d2467
Arto <myemail>
2022-06-18 12:57:30 -0300

    test(Metodos iniciais): :sparkles: Criado Função e teste que obtem o HTML do Qconcursos

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const URL = 'https://www.qconcursos.com/questoes-de-concursos/questoes/fabb3438-0c';
 describe('AppController', () => {
   let appController: AppController;
 
@@ -14,5 +15,13 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {});
+  describe('root', () => {
+    it('Deve retornar não nulo ao dar scrape no QConcursos', async () => {
+      const resposta = await appController.ObterQConcursos<any>(URL);
+
+      console.log(resposta);
+
+      expect(resposta).toBeTruthy();
+    });
+  });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import axios, { AxiosResponse } from 'axios';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +7,21 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  obterQConcursos(): string {
     return this.appService.getHello();
+  }
+
+  public async ObterQConcursos<T>(url: string): Promise<AxiosResponse<T>> {
+    let resultado: AxiosResponse<T>;
+    await axios(url)
+      .then((result: AxiosResponse<T>) => {
+        resultado = result;
+      })
+      .catch((err) => {
+        console.log(`Erro: ${err}`);
+        throw new Error(err.message);
+      });
+
+    return resultado;
   }
 }
